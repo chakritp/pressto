22c57156a78d9f080f44495c91328e1d381edd35
Chakrit Prasatwattana <chakritp888@gmail.com>
2017-11-06 11:48:04 -0800

    Set up react

---
+++ client/src/views/Menu.jsx
@@ -0,0 +1,44 @@
+import React from 'react'
+import axios from 'axios'
+
+class Menu extends React.Component {
+  state = {
+    menuItems: null
+  }
+
+  componentDidMount() {
+    axios({method: 'get', url: '/api/menu-items'})
+      .then(res => {
+        var menuItems = res.data.menuItems
+        console.log(menuItems)
+        this.setState({
+          menuItems: res.data.menuItems
+        })
+      })
+  }
+
+  render() {
+    return (
+      <div className="Menu">
+        { !this.state.menuItems
+        ? (
+            <div>Loading...</div>
+          ) 
+        : (
+            <div className="menuItems">
+              {this.state.menuItems.map(menuItem => {
+                return (
+                  <div key={menuItem._id}>
+                    {menuItem.name} <button>Add to Cart</button>
+                  </div>
+                )
+              })}
+            </div>
+          )
+        }
+      </div>
+    )
+  }
+}
+
+export default Menu
