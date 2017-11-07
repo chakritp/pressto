7a6a6be9de8fecb9a1c7ec2880a60938c251d27a
Chakrit Prasatwattana <chakritp888@gmail.com>
2017-11-06 17:00:09 -0800

    Save quantities to local storage

--- client/src/App.js
+++ client/src/App.js
@@ -7,13 +7,38 @@ import Home from './views/Home'
 import Menu from './views/Menu'
 
 class App extends Component {
+  state = {
+    shoppingCart: [] // { product, qty }
+  }
+  
+  // componentDidMount() {
+  //   this.setState({
+      
+  //   })
+  // }
+  
+  updateCart(item, quantity=1) {
+    // if the item already exists in local storage, add the inputted quantity to the existing quantity
+    var newQuantity = localStorage.getItem(item.name) ? Number(localStorage.getItem(item.name)) + Number(quantity) : quantity
+    localStorage.setItem(item.name, newQuantity)
+    console.log("Updated local storage")
+    // this.setState({
+    //   shoppingCart: [
+    //     ...this.state.shoppingCart, 
+    //     { item: item, quantity: quantity }
+    //   ]
+    // })
+  }
+
   render() {
     return (
       <div className="App">
         <Navbar />
         <Switch>
           <Route exact path="/" component={Home} />
-          <Route path="/menu" component={Menu} />
+          <Route path="/menu" render={(props) => {
+            return <Menu {...props} onUpdateCart={this.updateCart.bind(this)}/>
+          }} />
         </Switch>
       </div>
     );
